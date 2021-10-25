a6e1171a993e8c08b50c118aa00a5320d779d7d8
liu2015 <176784830@qq.com>
2021-10-25 09:20:53 +0800

    test

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ import random
 import time
 from selenium import webdriver
 
-with webdriver.Chrome() as driver:
+# with webdriver.Chrome() as driver:
+# win10 环境需要配置指向driver
+with webdriver.Chrome("C://chromedriver.exe") as driver:
+
     b = random.randint(-180, 180)
-    driver.get('https://www.baidu.com/')
+    driver.get('http://47.114.139.188/')
     print(b)
     # 随机填写内容
     date = driver.find_element_by_xpath(
-        '/html/body/div[1]/div[1]/div[5]/div/div/form/span[1]/input'
-    ).send_keys(b)
+        '/html/body/div/div/form/div/div[3]/input').send_keys("18111111111")
     time.sleep(1)
 
     # 触发点击事件
     driver.find_element_by_xpath(
-        '/html/body/div[1]/div[1]/div[5]/div[2]/div/form/span[2]/input').click(
-        )
+        '/html/body/div/div/form/div/div[4]/input').send_keys("123456")
     print(driver.title)
+
+    time.sleep(2)
+    driver.find_element_by_xpath("/html/body/div/div/form/div/button").click()
     time.sleep(5)
     print("递交分支到main1tef")
     print("递交")
